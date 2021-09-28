b9de6af608e8cd4f638479970b8462df8ba9bccb
Ian Yong <ianyongyc@gmail.com>
2021-09-28 15:44:40 +0800

    Set up router & middleware

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,20 +1,16 @@
 package main
 
 import (
-	"fmt"
+	"github.com/ianyong/todo-backend/internal/router"
 	"log"
 	"net/http"
-	"time"
 )
 
+// main is the entry point for the server.
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		_, err := fmt.Fprintf(w, "Hello World! The time now is %s.", time.Now().Format("2006-01-02 15:04:05"))
-		if err != nil {
-			log.Fatalln(err)
-		}
-	})
-	err := http.ListenAndServe(":8000", nil)
+	r := router.SetUp()
+
+	err := http.ListenAndServe(":8000", r)
 	if err != nil {
 		log.Fatalln(err)
 	}
---
+++ internal/router/router.go
@@ -0,0 +1,37 @@
+package router
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/ianyong/todo-backend/internal/handlers"
+	"time"
+)
+
+// SetUp sets up the middleware stack and routes for a chi.Router and returns it.
+func SetUp() chi.Router {
+	r := chi.NewRouter()
+	setUpMiddleware(r)
+	setUpRoutes(r)
+	return r
+}
+
+// setUpMiddleware sets up the middleware stack for a chi.Router.
+func setUpMiddleware(r chi.Router) {
+	// Injects a request ID in the context of each request.
+	r.Use(middleware.RequestID)
+	// Sets a http.Request's RemoteAddr to that of either the X-Forwarded-For or X-Real-IP header.
+	r.Use(middleware.RealIP)
+	// Logs the start and end of each request.
+	r.Use(middleware.Logger)
+	// Recovers from panics and return a 500 Internal Service Error.
+	r.Use(middleware.Recoverer)
+	// Returns a 504 Gateway Timeout after 1 min.
+	r.Use(middleware.Timeout(time.Minute))
+}
+
+// setUpRoutes sets up the routes for a chi.Router. All API routes are namespaced with '/api/v1'.
+func setUpRoutes(r chi.Router) {
+	r.Route("/api/v1", func(r chi.Router) {
+		r.Get("/", handlers.HelloWorld)
+	})
+}
