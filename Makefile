3d4c24388b30b23f99466d9e744a777b13d6cef0
Ian Yong <ianyongyc@gmail.com>
2021-09-28 17:26:29 +0800

    Add script to migrate database

--- Makefile
+++ Makefile
@@ -1,11 +1,16 @@
 BINARY_PATH=./bin/server/server.out
 SERVER_PATH=./cmd/server/main.go
 DB_CREATE_PATH=./cmd/database/create/main.go
+DB_MIGRATE_PATH=./cmd/database/migrate/main.go
 DB_DROP_PATH=./cmd/database/drop/main.go
 
 createdb:
 	@echo "Creating database..."
 	@go run ${DB_CREATE_PATH}
+
+migratedb:
+	@echo "Migrating database..."
+	@go run ${DB_MIGRATE_PATH}
 
 dropdb:
 	@echo "Dropping database..."
---
+++ cmd/database/migrate/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"github.com/ianyong/todo-backend/internal/config"
+	"github.com/ianyong/todo-backend/internal/database"
+	"log"
+	"github.com/rubenv/sql-migrate"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const (
+	directionEnvVar = "MIGRATION_DIRECTION"
+	stepsEnvVar = "MIGRATION_STEPS"
+	stepsNoLimit = 0
+)
+
+func main() {
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatalf("failed to load config: %v\n", err)
+	}
+
+	db, err := database.SetUp(cfg)
+	if err != nil {
+		log.Fatalf("failed to connect to database: %v\n", err)
+	}
+
+	migrations := &migrate.FileMigrationSource{
+		Dir: "migrations",
+	}
+
+	direction := migrate.Up
+	if dirEnvVarValue := os.Getenv(directionEnvVar); dirEnvVarValue != "" {
+		switch strings.ToLower(dirEnvVarValue) {
+		case "up":
+			direction = migrate.Up
+		case "down":
+			direction = migrate.Down
+		default:
+			log.Fatalf("unrecognised value for %s: '%s'\n", directionEnvVar, dirEnvVarValue)
+		}
+	}
+
+	maxSteps := stepsNoLimit
+	if stepsEnvVarValue := os.Getenv(stepsEnvVar); stepsEnvVarValue != "" {
+		maxSteps, err = strconv.Atoi(stepsEnvVarValue)
+		if err != nil {
+			log.Fatalf("unrecognised value for %s: '%s'", stepsEnvVar, stepsEnvVarValue)
+		}
+	}
+
+	steps, err := migrate.ExecMax(db.DB, "postgres", migrations, direction, maxSteps)
+	if err != nil {
+		log.Fatalf("failed to migrate database: %v\n", err)
+	}
+
+	if direction == migrate.Up {
+		log.Printf("Applied %d migrations!\n", steps)
+	} else {
+		log.Printf("Reverted %d migrations!\n", steps)
+	}
+}
