c14e8e09d06e1d7f0c31ecbc33a40188d6fcce3b
Ian Yong <ianyongyc@gmail.com>
2021-09-28 19:09:36 +0800

    Unexport fields of todo domain model

--- internal/core/domainmodels/todo.go
+++ internal/core/domainmodels/todo.go
@@ -4,8 +4,25 @@ import "time"
 
 // Todo represents a task that needs to be done.
 type Todo struct {
-	Name        string
-	Description string
-	DueDate     time.Time
-	IsCompleted bool
+	name        string
+	description string
+	dueDate     time.Time
+	isCompleted bool
+}
+
+func NewTodo(name string, description string, dueDate time.Time) *Todo {
+	return &Todo{
+		name:        name,
+		description: description,
+		dueDate:     dueDate,
+		isCompleted: false,
+	}
+}
+
+func (t *Todo) SetComplete() {
+	t.isCompleted = true
+}
+
+func (t *Todo) SetIncomplete() {
+	t.isCompleted = false
 }
