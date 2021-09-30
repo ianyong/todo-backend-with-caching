8697e8543b747f689ea823c21ce614644f7e6e8b
Ian Yong <ianyongyc@gmail.com>
2021-09-30 14:23:40 +0800

    Add create todo handler

---
+++ internal/adapters/userinterface/handlers/todohandlers/create.go
@@ -0,0 +1,39 @@
+package todohandlers
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/ianyong/todo-backend/internal/adapters/userinterface/api"
+	"github.com/ianyong/todo-backend/internal/adapters/userinterface/json"
+	"github.com/ianyong/todo-backend/internal/adapters/userinterface/params/todoparams"
+	"github.com/ianyong/todo-backend/internal/adapters/userinterface/views/todoviews"
+	"github.com/ianyong/todo-backend/internal/services"
+)
+
+func Create(r *http.Request, s *services.Services) (*api.Response, error) {
+	var createParams todoparams.CreateParams
+	err := json.DecodeParams(r.Body, &createParams)
+	if err != nil {
+		return nil, fmt.Errorf("unable to decode request body into params: %w", err)
+	}
+
+	todo := createParams.ToModel()
+
+	err = s.TodoService.AddTodo(todo)
+	if err != nil {
+		return nil, err
+	}
+
+	todoView := todoviews.ViewFrom(todo)
+
+	data, err := json.EncodeView(todoView)
+	if err != nil {
+		return nil, err
+	}
+
+	return &api.Response{
+		Payload: data,
+		Code:    http.StatusCreated,
+	}, nil
+}
--- internal/adapters/userinterface/routes/todo.go
+++ internal/adapters/userinterface/routes/todo.go
@@ -4,16 +4,17 @@ import (
 	"github.com/go-chi/chi/v5"
 
 	"github.com/ianyong/todo-backend/internal/adapters/userinterface/api"
-	todohandlers2 "github.com/ianyong/todo-backend/internal/adapters/userinterface/handlers/todohandlers"
+	"github.com/ianyong/todo-backend/internal/adapters/userinterface/handlers/todohandlers"
 	"github.com/ianyong/todo-backend/internal/services"
 )
 
 func GetTodoRoutes(s *services.Services) func(r chi.Router) {
 	return func(r chi.Router) {
-		r.Get("/", api.WrapHandler(s, todohandlers2.List))
+		r.Get("/", api.WrapHandler(s, todohandlers.List))
+		r.Post("/", api.WrapHandler(s, todohandlers.Create))
 		r.Route("/{id}", func(r chi.Router) {
-			r.Get("/", api.WrapHandler(s, todohandlers2.Read))
-			r.Delete("/", api.WrapHandler(s, todohandlers2.Destroy))
+			r.Get("/", api.WrapHandler(s, todohandlers.Read))
+			r.Delete("/", api.WrapHandler(s, todohandlers.Destroy))
 		})
 	}
 }
