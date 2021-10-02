5b21d77a53f3a2176aaba47425ff974f63bcbf42
Ian Yong <ianyongyc@gmail.com>
2021-10-02 21:36:17 +0800

    Add tests for update handler

--- internal/adapters/userinterface/handlers/todohandlers_test/update_test.go
+++ internal/adapters/userinterface/handlers/todohandlers_test/update_test.go
@@ -12,32 +12,41 @@ import (
 
 	"github.com/ianyong/todo-backend/internal/adapters/userinterface/views/todoviews"
 	"github.com/ianyong/todo-backend/internal/tests"
+	"github.com/ianyong/todo-backend/internal/tests/testseeds"
 )
 
-type CreateTodoTestSuite struct {
+type UpdateTodoTestSuite struct {
 	suite.Suite
 	tests.TestComponents
 }
 
-func (s *CreateTodoTestSuite) SetupTest() {
+func (s *UpdateTodoTestSuite) SetupTest() {
 	s.TestComponents = tests.SetUp()
 }
 
-const validCreateJSON = `{
+const validUpdateJSON = `{
+    "id": 2,
     "name": "Todo",
     "description": "This is a valid todo.",
-    "dueDate": "2021-11-01T00:00:00.000000+00:00"
+    "dueDate": "2021-11-01T00:00:00.000000+00:00",
+    "isCompleted": true
 }`
 
-var createdTodo = todoviews.View{
-	ID:          1,
+var updatedTodo = todoviews.View{
+	ID:          2,
 	Name:        "Todo",
 	Description: "This is a valid todo.",
 	DueDate:     time.Date(2021, time.November, 1, 0, 0, 0, 0, time.UTC).Local(),
+	IsCompleted: true,
 }
 
-func (s *CreateTodoTestSuite) TestCreateTodo() {
-	request, err := http.NewRequest(http.MethodPost, "/api/v1/todos", strings.NewReader(validCreateJSON))
+func (s *CreateTodoTestSuite) TestUpdateTodo() {
+	err := testseeds.SeedTodos(s.DB)
+	if err != nil {
+		s.T().Errorf("Error seeding todos: %v", err)
+	}
+
+	request, err := http.NewRequest(http.MethodPut, "/api/v1/todos/2", strings.NewReader(validUpdateJSON))
 	if err != nil {
 		s.T().Errorf("Error creating request: %v", err)
 	}
@@ -45,7 +54,7 @@ func (s *CreateTodoTestSuite) TestCreateTodo() {
 	responseRecorder := httptest.NewRecorder()
 	s.Router.ServeHTTP(responseRecorder, request)
 
-	tests.CheckResponseCode(s.T(), http.StatusCreated, responseRecorder.Code)
+	tests.CheckResponseCode(s.T(), http.StatusOK, responseRecorder.Code)
 
 	body := tests.GetResponseBody(s.T(), responseRecorder.Body)
 	var actualTodo todoviews.View
@@ -54,7 +63,7 @@ func (s *CreateTodoTestSuite) TestCreateTodo() {
 		s.T().Errorf("Error decoding response body: %v", err)
 	}
 
-	expectedTodo := createdTodo
+	expectedTodo := updatedTodo
 
 	tests.CheckResponseBody(s.T(), expectedTodo, actualTodo)
 
@@ -64,13 +73,13 @@ func (s *CreateTodoTestSuite) TestCreateTodo() {
 	}
 }
 
-const missingFieldCreateJSON = `{
-    "name": "Todo",
-    "dueDate": "2021-11-01T00:00:00.000000+00:00"
-}`
+func (s *CreateTodoTestSuite) TestUpdateTodoIDMismatch() {
+	err := testseeds.SeedTodos(s.DB)
+	if err != nil {
+		s.T().Errorf("Error seeding todos: %v", err)
+	}
 
-func (s *CreateTodoTestSuite) TestCreateTodoMissingField() {
-	request, err := http.NewRequest(http.MethodPost, "/api/v1/todos", strings.NewReader(missingFieldCreateJSON))
+	request, err := http.NewRequest(http.MethodPut, "/api/v1/todos/3", strings.NewReader(validUpdateJSON))
 	if err != nil {
 		s.T().Errorf("Error creating request: %v", err)
 	}
@@ -97,15 +106,20 @@ func (s *CreateTodoTestSuite) TestCreateTodoMissingField() {
 	}
 }
 
-const extraFieldCreateJSON = `{
-    "id": 3,
+const missingFieldUpdateJSON = `{
+    "id": 2,
     "name": "Todo",
     "description": "This is a valid todo.",
     "dueDate": "2021-11-01T00:00:00.000000+00:00"
 }`
 
-func (s *CreateTodoTestSuite) TestCreateTodoExtraField() {
-	request, err := http.NewRequest(http.MethodPost, "/api/v1/todos", strings.NewReader(extraFieldCreateJSON))
+func (s *CreateTodoTestSuite) TestUpdateTodoMissingField() {
+	err := testseeds.SeedTodos(s.DB)
+	if err != nil {
+		s.T().Errorf("Error seeding todos: %v", err)
+	}
+
+	request, err := http.NewRequest(http.MethodPut, "/api/v1/todos/2", strings.NewReader(missingFieldUpdateJSON))
 	if err != nil {
 		s.T().Errorf("Error creating request: %v", err)
 	}
@@ -132,6 +146,48 @@ func (s *CreateTodoTestSuite) TestCreateTodoExtraField() {
 	}
 }
 
-func TestCreateTodoTestSuite(t *testing.T) {
-	suite.Run(t, new(CreateTodoTestSuite))
+const extraFieldUpdateJSON = `{
+    "id": 2,
+    "name": "Todo",
+    "hello": "world",
+    "description": "This is a valid todo.",
+    "dueDate": "2021-11-01T00:00:00.000000+00:00",
+    "isCompleted": true
+}`
+
+func (s *CreateTodoTestSuite) TestUpdateTodoExtraField() {
+	err := testseeds.SeedTodos(s.DB)
+	if err != nil {
+		s.T().Errorf("Error seeding todos: %v", err)
+	}
+
+	request, err := http.NewRequest(http.MethodPut, "/api/v1/todos/2", strings.NewReader(extraFieldUpdateJSON))
+	if err != nil {
+		s.T().Errorf("Error creating request: %v", err)
+	}
+
+	responseRecorder := httptest.NewRecorder()
+	s.Router.ServeHTTP(responseRecorder, request)
+
+	tests.CheckResponseCode(s.T(), http.StatusBadRequest, responseRecorder.Code)
+
+	body := tests.GetResponseBody(s.T(), responseRecorder.Body)
+	var actualTodo todoviews.View
+	err = json.Unmarshal(body.Payload, &actualTodo)
+	if err != nil {
+		s.T().Errorf("Error decoding response body: %v", err)
+	}
+
+	expectedTodo := todoviews.View{}
+
+	tests.CheckResponseBody(s.T(), expectedTodo, actualTodo)
+
+	err = s.TruncateTables("todos")
+	if err != nil {
+		s.T().Errorf("Error truncating tables: %v", err)
+	}
+}
+
+func TestUpdateTodoTestSuite(t *testing.T) {
+	suite.Run(t, new(UpdateTodoTestSuite))
 }
