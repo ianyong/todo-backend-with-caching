4a851ae3278d2c2c80636f21fb788ba276cad2b6
Ian Yong <ianyongyc@gmail.com>
2021-10-02 11:13:15 +0800

    Make naming of variables in tests clearer

--- internal/adapters/userinterface/handlers/todohandlers/list_test.go
+++ internal/adapters/userinterface/handlers/todohandlers/list_test.go
@@ -21,30 +21,6 @@ func TestMain(m *testing.M) {
 	os.Exit(exitCode)
 }
 
-func TestListTodosEmptyCollection(t *testing.T) {
-	request, err := http.NewRequest("GET", "/api/v1/todos", nil)
-	if err != nil {
-		t.Errorf("Error creating request: %v", err)
-	}
-
-	responseRecorder := httptest.NewRecorder()
-	handler := api.WrapHandler(testComponents.Services, List)
-	handler.ServeHTTP(responseRecorder, request)
-
-	tests.CheckResponseCode(t, http.StatusOK, responseRecorder.Code)
-
-	body := tests.GetResponseBody(t, responseRecorder.Body)
-	var todos []todoviews.ListView
-	err = json.Unmarshal(body.Payload, &todos)
-	if err != nil {
-		t.Errorf("Error decoding response body: %v", err)
-	}
-
-	todoListViews := []todoviews.ListView{}
-
-	tests.CheckResponseBody(t, todoListViews, todos)
-}
-
 func TestListTodos(t *testing.T) {
 	err := testseeds.SeedTodos(testComponents.DB)
 	if err != nil {
@@ -63,22 +39,46 @@ func TestListTodos(t *testing.T) {
 	tests.CheckResponseCode(t, http.StatusOK, responseRecorder.Code)
 
 	body := tests.GetResponseBody(t, responseRecorder.Body)
-	var todos []todoviews.ListView
-	err = json.Unmarshal(body.Payload, &todos)
+	var actualTodos []todoviews.ListView
+	err = json.Unmarshal(body.Payload, &actualTodos)
 	if err != nil {
 		t.Errorf("Error decoding response body: %v", err)
 	}
 
-	todoListViews := make([]todoviews.ListView, len(testseeds.TodoSeeds))
+	expectedTodos := make([]todoviews.ListView, len(testseeds.TodoSeeds))
 	for i := range testseeds.TodoSeeds {
 		todo := testseeds.TodoSeeds[i]
-		todoListViews[i] = todoviews.ListViewFrom(&todo)
+		expectedTodos[i] = todoviews.ListViewFrom(&todo)
 	}
 
-	tests.CheckResponseBody(t, todoListViews, todos)
+	tests.CheckResponseBody(t, expectedTodos, actualTodos)
 
 	err = testComponents.TruncateTables("todos")
 	if err != nil {
 		t.Errorf("Error truncating tables: %v", err)
 	}
+}
+
+func TestListTodosEmptyCollection(t *testing.T) {
+	request, err := http.NewRequest("GET", "/api/v1/todos", nil)
+	if err != nil {
+		t.Errorf("Error creating request: %v", err)
+	}
+
+	responseRecorder := httptest.NewRecorder()
+	handler := api.WrapHandler(testComponents.Services, List)
+	handler.ServeHTTP(responseRecorder, request)
+
+	tests.CheckResponseCode(t, http.StatusOK, responseRecorder.Code)
+
+	body := tests.GetResponseBody(t, responseRecorder.Body)
+	var actualTodos []todoviews.ListView
+	err = json.Unmarshal(body.Payload, &actualTodos)
+	if err != nil {
+		t.Errorf("Error decoding response body: %v", err)
+	}
+
+	expectedTodos := []todoviews.ListView{}
+
+	tests.CheckResponseBody(t, expectedTodos, actualTodos)
 }
